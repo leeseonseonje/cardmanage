c3d7e9c8a5a6a3778ab9eb9c29f7220992c6b51f
leeseonseonje <leeseonje9323@gamail.com>
2021-06-07 02:48:19 +0900

    card

--- src/main/java/cardmanage/cardmanage/controller/HomeController.java
+++ src/main/java/cardmanage/cardmanage/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping("")
     public String home() {
         return "home";
     }
